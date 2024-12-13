306ac08989098dd8844efc4e505f017ccb036a01
Pnbcris <118472852+Pnbcris@users.noreply.github.com>
2024-12-13 14:02:55 -0800

    added input

--- classwork.cpp
+++ classwork.cpp
@@ -4,6 +4,12 @@
 int main()
 {
   // write code here
+
+  std::string x;
+  std::cout << "what is your name?" << std::endl;
+  std::cin >> x;
+
+  std:cout << "Hello " << x << ". It is nice to meet you!" >> std::endl;
   
   return 0;
 }
